ae2db2101ef3acf7a099614c314a69f190e0a2e2
Hewerton Moreira <79805868+hewmoreira@users.noreply.github.com>
2021-04-06 19:53:18 -0300

    Add files via upload

---
+++ Aula 08 - Trunfo, imagens, manipulação do HTML e CSS/README.markdown
@@ -0,0 +1,5 @@
+# Aula 8: Trunfo, imagens, manipulação do HTML e CSS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/hewmoreira/pen/WNROzMa](https://codepen.io/hewmoreira/pen/WNROzMa).
+
+
---
+++ Aula 08 - Trunfo, imagens, manipulação do HTML e CSS/dist/script.js
@@ -0,0 +1,138 @@
+let kakashi = {
+  nome: 'Kakashi',
+  imagem: 'https://avatarfiles.alphacoders.com/524/thumb-52442.jpg',
+  atributos: {
+  taijutsu: 60,
+  ninjutsu: 75,
+  genjutsu: 55    
+  }
+}
+
+let naruto = {
+  nome: 'Naruto',
+  imagem: 'https://i.pinimg.com/originals/2a/92/06/2a9206a4a0d1d23cf92636c42115d054.jpg',
+  atributos: {
+  taijutsu: 55,
+  ninjutsu: 80,
+  genjutsu: 40    
+  }
+}
+
+let sakura = {
+  nome: 'Sakura',
+  imagem: 'https://avatarfiles.alphacoders.com/891/89187.jpg',
+  atributos: {
+  taijutsu: 85,
+  ninjutsu: 60,
+  genjutsu: 55    
+  }
+}
+
+let sasuke = {
+  nome: "Sasuke",
+  imagem: 'https://avatarfiles.alphacoders.com/101/thumb-1920-101899.jpg',
+  atributos: {
+  taijutsu: 70,
+  ninjutsu: 90,
+  genjutsu: 75      
+  }
+}
+
+let zabuza = {
+  nome: 'Zabuza',
+  imagem: 'https://i.pinimg.com/originals/51/a3/aa/51a3aa8dd76f874d2596e067213cd15b.jpg',
+  atributos: {
+  taijutsu: 60,
+  ninjutsu: 40,
+  genjutsu: 60    
+  }
+}
+
+let haku = {
+  nome: 'Haku',
+  imagem: 'https://64.media.tumblr.com/9fc589638791b25a19a3beb2bceb7ead/tumblr_ps0dqz4ZTX1qg53joo1_1280.png',
+  atributos: {
+  taijutsu: 40,
+  ninjutsu: 70,
+  genjutsu: 70    
+  }
+}
+
+let cartaMaquina
+let cartaJogador
+let cartas = [kakashi, naruto, sakura, sasuke, zabuza, haku]
+
+function sortearCarta() {
+  let numeroCartaMaquina = parseInt(Math.random() * 3)
+  cartaMaquina = cartas[numeroCartaMaquina]
+  
+  let numeroCartaJogador = parseInt(Math.random() * 3)
+  while (numeroCartaJogador == numeroCartaMaquina){
+    numeroCartaJogador = parseInt(Math.random() * 3)
+  }
+  
+  cartaJogador = cartas[numeroCartaJogador]
+  console.log(cartaJogador)
+  
+  document.getElementById('btnSortear').disabled = true
+  document.getElementById('btnJogar').disabled = false  
+  
+  exibeCartaJogador()
+}
+
+function exibeCartaJogador(){
+  let divCartaJogador = document.getElementById('carta-jogador')
+  let moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+  let nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+  let opcoesTexto = ''
+  
+  for (var atributo in cartaJogador.atributos){
+    opcoesTexto += `<input type='radio' name='atributo' value='${atributo}'>` + atributo + " " + cartaJogador.atributos[atributo] + "<br>"
+  }
+
+  let html = '<div id="opcoes" class="carta-status">'
+  
+  divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    let radioAtributo = document.getElementsByName('atributo')
+    for (let i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+  let divResultado = document.getElementById('resultado')
+  var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu!</p>'
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu!</p>'
+    } else {
+        htmlResultado = '<p class="resultado-final">Empate!</p>'
+    }
+  divResultado.innerHTML = htmlResultado
+  
+  exibeCartaMaquina()
+}
+
+function exibeCartaMaquina() {
+  let divCartaMaquina = document.getElementById('carta-maquina')
+  let moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+  let nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+  let opcoesTexto = ''
+  
+  for (var atributo in cartaMaquina.atributos){
+    opcoesTexto += `<p type='text' name='atributo' value='${atributo}'>` + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+  }
+
+  let html = '<div id="opcoes" class="carta-status">'
+  
+  divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'  
+}
